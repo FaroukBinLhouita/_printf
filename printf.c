52ef2bef82ed04613dcab27d2a3aa6d2dda30fc5
farouk <faroukhoty@gmail.com>
2023-10-15 08:28:12 +0300

    changes

--- printf.c
+++ printf.c
@@ -8,59 +8,61 @@
  */
 int _printf(const char *format, ...)
 {
-int count = 0;
+    int count = 0;
+    va_list list;
 
+    if (format == NULL)
+    {
+        return (-1);
+    }
 
-if (format == NULL)
-{
-return (-1);
-}
+    va_start(list, format);
 
-va_list list;
-va_start (list, format);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            count++;
+        }
+        else
+        {
+            format++;
+            if (*format == '\0')
+            {
+                break;
+            }
 
-while (*format)
-{
-if (*format != '%')
-{
-write(1, format, 1);
-count++;
-}
-else
-{
-format++;
-if (*format == '\0')
-{
-break;
-}
+            if (*format == '%')
+            {
+                write(1, format, 1);
+                count++;
+            }
+            else if (*format == 'c')
+            {
+                char c = va_arg(list, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*format == 's')
+            {
+            int i = 0;
+            char *string = va_arg(list, char*);
 
-if (*format == '%')
-{
-write(1, format, 1);
-count++;
-}
-else if (*format == 'c')
-{
-char c = va_arg(list, int);
-write(1, &c, 1);
-count++;
-}
-else if (*format == 's')
-{
-int i;
-char *string = va_arg(list, char*);
+            while (string[i] != '\0')
+            {
+                i++;
+            }
+            
+            write(1, string, i);
+            count += i;
+            }
+        }
 
-for (i = 0; string[i] != '\0'; i++)
-;
-write(1, string, i);
-count += i;
-}
-}
+    format++;
+    }
 
-format++;
-}
+    va_end(list);
 
-va_end(list);
-
-return (count);
+    return (count);
 }
