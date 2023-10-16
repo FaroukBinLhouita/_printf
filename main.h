c0dfc8df9e79849eeb3854a360866fc5a3857429
FaroukBinLhouita <133813472+FaroukBinLhouita@users.noreply.github.com>
2023-10-16 08:45:58 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,15 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define SIZE 1024
+
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
+
 
 int _printf(const char *format, ...);
-char *buffer_decimal(int num);
-char buffer_char(va_list argument);
-char *buffer_str(va_list argument);
-void handle_int_recursive(int number, char *buffer, int *index);
 
 #endif
