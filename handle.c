8990e421861bb7a4a4de8615f7cd0f20e9cc56eb
farouk <faroukhoty@gmail.com>
2023-10-16 21:47:03 +0300

    comiit

--- handle.c
+++ handle.c
@@ -1,10 +1,10 @@
-#include "main.h"
+/*#include "main.h"
 
 /**
  * buffer_decimal - get the integer and convert to string
  * @num: get the integer
  * Return: return a pointer to a string
- */
+
 char *buffer_decimal(int num)
 {
 int size, temp, i;
@@ -39,7 +39,7 @@ return result;
  * buffer_char - get the char element
  * @va_arg: get the emlment
  * Return: return a varabile has the element
- */
+
 char buffer_char(va_list argument)
 {
 char c = va_arg(argument, int);
@@ -50,7 +50,7 @@ return (c);
  * buffer_str - get the string
  * @va_arg: get the string
  * Return: return a varabile has the string
- */
+
 char *buffer_str(va_list argument)
 {
 char *str = va_arg(argument, char*);
@@ -62,7 +62,7 @@ return (str);
  * @number: the integer to convert
  * @buffer: the buffer to store the string
  * @index: pointer to the current index in the buffer
- */
+
 void handle_int_recursive(int number, char *buffer, int *i)
 {
 if (number == 0)
@@ -74,3 +74,89 @@ handle_int_recursive(n, buffer, i);
 buffer[*i] = '0' + (number % 10);
 (*i)++;
 }
+
+
+#include "main.h"
+
+/**
+ * _printf - my printf function
+ *
+ * @format: the string should print
+ * Return: an intager
+
+int _printf(const char *format, ...)
+{
+char buffer[SIZE];
+int index = 0;
+va_list list;
+
+if (format == NULL)
+{
+return (-1);
+}
+
+va_start(list, format);
+
+while (*format)
+{
+if (index == SIZE - 1)
+{
+buffer[index] = '\0';
+write(1, buffer, index);
+index = 0;
+}
+
+if (*format != '%')
+{
+buffer[index] = *format;
+index++;
+}
+
+else
+{
+format++;
+if (*format == 's')
+{
+int i;
+char *tmp = buffer_str(list);
+for (i = 0; tmp[i] != '\0'; i++)
+{
+    buffer[index] = tmp[i];
+    index++;
+}
+}
+else if (*format == 'c')
+{
+buffer[index] = buffer_char(list);
+index++;
+}
+else if (*format == 'i' || *format == 'd')
+{
+int num = va_arg(list, int);
+char *num_str = buffer_decimal(num);
+while (*num_str)
+{
+    buffer[index] = *num_str;
+    num_str++;
+    index++;
+}
+}
+else if (*format == '%')
+{
+buffer[index] = *format;
+index++;
+}
+}
+format++;
+}
+
+if (index > 0)
+{
+buffer[index] = '\0';
+write(1, buffer, index);
+}
+
+va_end(list);
+return (index);
+}
+*/
--- main.h
+++ main.h
@@ -1,14 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define SIZE 1024
+
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
 
 int _printf(const char *format, ...);
-char *buffer_decimal(int num);
-char buffer_char(va_list argument);
-char *buffer_str(va_list argument);
-void handle_int_recursive(int number, char *buffer, int *index);
 
 #endif
