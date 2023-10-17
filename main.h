db1aad7960eb086c00fbe9cfa6428d4887693584
farouk <faroukhoty@gmail.com>
2023-10-17 03:13:39 +0300

    task 0

--- main.h
+++ main.h
@@ -1,6 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int print_c(va_list arg);
+int _printf(const char *format, ...);
+void handle_int(int number);
+
+#endif
+
+/*
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
 
@@ -9,3 +24,4 @@ int _putchar(char c);
 void convert_int(int number);
 
 #endif
+*/
