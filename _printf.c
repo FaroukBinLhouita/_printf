3a5000dfab8ef08a9a600c91be843b69f99c5e15
farouk <faroukhoty@gmail.com>
2023-10-17 21:37:51 +0300

    0

--- _printf.c
+++ _printf.c
@@ -49,7 +49,7 @@ count += _puts(string);
 else if (format[j] == 'd' || format[j] == 'i')
 {
 int num = va_arg(list, int);
-int len = _itoa(num, num_str);
+int value = _itoa(num, num_str);
 count += _puts(num_str);
 }
 }
